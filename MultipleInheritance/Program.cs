4ac33d194f025a51f21bb56eae46291485abe978
altherrdeen <110594569+altherrdeen@users.noreply.github.com>
2022-10-07 10:56:17 +0200

    Create Program.cs

---
+++ MultipleInheritance/Program.cs
@@ -0,0 +1,37 @@
+namespace MultipleInheritanceInterfaces
+{
+    public interface IProduct
+    {
+        public void Sell();
+    }
+
+    public interface IDiscount
+    {
+        public void AdvertiseDiscount();
+    }
+
+    public class Chair : IDiscount, IProduct
+    {
+        public string Name { get; set; }
+
+        public void Sell()
+        {
+            Console.WriteLine("The chair was sold");
+        }
+
+        public void AdvertiseDiscount()
+        {
+            Console.WriteLine("This chair is on sale");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Chair chair = new Chair();
+            chair.Sell();
+            chair.AdvertiseDiscount();
+        }
+    }
+}
