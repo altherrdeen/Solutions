1a2ee5be4d6bc7ffb4ad61aaf3693c422979f911
altherrdeen <110594569+altherrdeen@users.noreply.github.com>
2022-10-11 17:20:43 +0200

    Create README.cs

---
+++ StringBuilder/README.cs
@@ -0,0 +1,8 @@
+Unterschiede String und StringBuilder:
+
+-mutable, immutable
+-threading
+-stringbuilder sind objekte einer Klasse
+-stringbuilder können null sein
+-stringbuilder haben extrafunktionen
+-StringBuilder sind deutlich schneller darin, strings zusammenzufügen
