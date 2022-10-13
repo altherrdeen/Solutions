76778844e8b425962a4d078334fb834164f89575
altherrdeen <110594569+altherrdeen@users.noreply.github.com>
2022-10-13 14:16:01 +0200

    Update Program.cs

--- StringBuilder/Program.cs
+++ StringBuilder/Program.cs
@@ -8,33 +8,34 @@ namespace StringBuilderExample
         {
             //Insert
             StringBuilder stringBuilderInsert = new StringBuilder();
-            stringBuilderInsert.Insert(0, "baum", 50);
+            stringBuilderInsert.Insert(0, "baum", 50); //Insert "baum" 50 times at position 0
             Console.WriteLine(stringBuilderInsert.ToString());
 
             //Append and Remove
             StringBuilder stringBuilderRemove = new StringBuilder();
             stringBuilderRemove.Append("abcdefg");
-            stringBuilderRemove.Remove(2, 3); //An 2ter Stelle 3 characters removen
+            stringBuilderRemove.Remove(2, 3); //Remove 3 characters at position 2
             Console.WriteLine(stringBuilderRemove.ToString());
 
             //Replace
             StringBuilder stringBuilderReplace = new StringBuilder();
             stringBuilderReplace.Append("John Smith");
-            stringBuilderReplace.Replace("John", "James");
+            stringBuilderReplace.Replace("John", "James"); //Replace "John" with "James"
             Console.WriteLine(stringBuilderReplace.ToString());
 
 
 
             //Mutable VS Immutable
             string exampleString = "a value";
-            Console.WriteLine(exampleString.GetHashCode());
+            Console.WriteLine("Hash 1 of a string: " + exampleString.GetHashCode());
             exampleString = "a different value";
-            Console.WriteLine(exampleString.GetHashCode()); //Different Hash because strings are mutable
+            Console.WriteLine("Hash 2 of a string: " + exampleString.GetHashCode());
 
             StringBuilder stringBuilderImmutable = new StringBuilder();
-            Console.WriteLine(stringBuilderImmutable.GetHashCode());
+            Console.WriteLine("Hash 1 of a stringbuilder: " + stringBuilderImmutable.GetHashCode());
             stringBuilderImmutable.Append("abcdefg");
-            Console.WriteLine(stringBuilderImmutable.GetHashCode()); //Same Hash because StringBuilders are immutable
+            Console.WriteLine("Hash 2 of a stringbuilder: " + stringBuilderImmutable.GetHashCode());
+            
         }
     }
 }
