7f67f88c9d92f249422451f651c3164a3243a4d2
altherrdeen <110594569+altherrdeen@users.noreply.github.com>
2022-12-09 11:38:55 +0100

    Update SingletonExample.cs

--- Singleton VS Static/SingletonExample.cs
+++ Singleton VS Static/SingletonExample.cs
@@ -9,14 +9,14 @@ namespace SingletonVSStatic
     public class SingletonExample : IExampleInterface //Implementing interface/class throws no exception
     {
         private string ExampleVariable = String.Empty; //Declaring non-static variable throws no exception
-        private static readonly SingletonExample _instance = new SingletonExample(); 
+        
+        private static readonly SingletonExample _instance = new SingletonExample();
+        public bool ExampleMethod() { return true; } //Non-static method throws no exception
         private SingletonExample() { }
 
         public static SingletonExample GetInstance()
         {
             return _instance;
         }
-
-        public bool ExampleMethod() { return true; }
     }
 }
